4792c792e18544be2298f9728425c9f9ee8d30dc
Yusadolat <yusadolat@gmail.com>
2017-10-19 13:56:01 +0000

    More names Added

---
+++ loop2.py
@@ -0,0 +1,5 @@
+student_names = ["Idrees", "Hamzah", "Opson", "Rasheed", "Pelawi", "Ajao", "Aduke"]
+
+for student in student_names:
+    if student == "Ajao":
+        print("Wawu I found " + student)
