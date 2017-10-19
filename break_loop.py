d809222ac60abe215f91f6e8096dbadf2017add8
Yusadolat <yusadolat@gmail.com>
2017-10-19 14:04:40 +0000

    break statement with loop

---
+++ break_loop.py
@@ -0,0 +1,8 @@
+student_names = ["Idrees", "Hamzah", "Opson", "Rasheed", "Pelawi", "Ajao", "Aduke"]
+
+for student in student_names:
+    if student == "Ajao":
+        print("Wawu I found " + student)
+        break
+    print("Currently testing " +student)
+
