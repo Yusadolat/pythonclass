0cfd04bbca36e34ca5b5c1c43f31e127627f1996
Yusadolat <yusadolat@gmail.com>
2017-10-19 14:08:06 +0000

    continue statement with loop

---
+++ continue_loop.py
@@ -0,0 +1,8 @@
+student_names = ["Idrees", "Hamzah", "Opson", "Rasheed", "Pelawi", "Ajao", "Aduke"]
+
+for student in student_names:
+    if student == "Ajao":
+        continue #this will skip the name ajao if found
+
+
+
